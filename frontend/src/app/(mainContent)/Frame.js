ded4a2e8c3f211e50e912bdd7f7ce79f35332ea0
SMLDN <sakmyapeo@gmail.com>
2024-02-09 05:21:54 +0900

    toi uu anh

--- frontend/src/app/(mainContent)/Frame.js
+++ frontend/src/app/(mainContent)/Frame.js
@@ -2,7 +2,7 @@ export function useFrame(switchTag, sideBar, infoFrame, mainFrame) {
   return (
     <>
       <div>{switchTag}</div>
-      <div className="grow1 w-1/5 mt-10 mb-14 overflow-auto">{sideBar}</div>
+      <div className="grow1 w-1/5 mt-10 mb-16 overflow-auto">{sideBar}</div>
       <div className="grow1 pl-7 pr-7 w-1/4 mt-12 mb-14 overflow-auto text-zinc-900 font-semibold tracking-wider">
         {infoFrame}
       </div>
--- frontend/src/app/(mainContent)/layout.js
+++ frontend/src/app/(mainContent)/layout.js
@@ -1,11 +1,22 @@
 import Nav from "@/components/Nav";
 import "./mainContent.css";
+import Image from "next/image";
 
 export default function MainContentLayout({ children }) {
   return (
     <>
       <Nav />
-      <div className="main-container">{children}</div>
+
+      <div className="main-container">
+        <Image
+          src="/static/img/ui/bg4.png"
+          width={1113}
+          height={711}
+          className="absolute -z-10"
+          alt="cửu âm đại điển"
+        ></Image>
+        {children}
+      </div>
     </>
   );
 }

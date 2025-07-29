77c047920faff97148ed701de3a33a6e0cb55ac8
SMLDN <sakmyapeo@gmail.com>
2025-07-29 09:21:43 +0900

    a

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,20 +1,29 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import StoreProvider from "./StoreProvider";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["vietnamese"] });
 
 export default function RootLayout({ children }) {
   return (
     <StoreProvider>
-    <html lang="vi">
-      <body className={inter.className}>
-        {children}
-        <div className="text-right mr-10 text-white">ZDN@2025</div>
-      </body>
-      <GoogleAnalytics gaId="G-7X3R39YR6Z" />
-    </html>
+      <html lang="vi">
+        <head>
+          <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5491378674308309"
+            crossOrigin="anonymous"
+            strategy="afterInteractive"
+          />
+        </head>
+        <body className={inter.className}>
+          {children}
+          <div className="text-right mr-10 text-white">ZDN@2025</div>
+        </body>
+        <GoogleAnalytics gaId="G-7X3R39YR6Z" />
+      </html>
     </StoreProvider>
   );
 }
